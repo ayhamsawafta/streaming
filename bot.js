82346cccb9c23dee20c9d4e0d1c016540eed67f7
ayhamsawafta <43705668+ayhamsawafta@users.noreply.github.com>
2018-11-14 08:26:23 -0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,78 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('By Wenteed')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+
+  
+  
+  
+ 
+  
+  
+  
+  
+  
+  });
+  
+   
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+const adminprefix = "D"; 
+const devs = ['480479617237516299'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+   
+if (message.content.startsWith(adminprefix + 'sg')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} :white_check_mark:  **`)
+} else
+  if (message.content.startsWith(adminprefix + 'sn')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغير اسمك :white_check_mark: `)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'sa')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغيير صورتك :white_check_mark: `);
+      } else    
+if (message.content.startsWith(adminprefix + 'st')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**:white_check_mark:    ${argresult}**`)
+}
+ 
+});
+ 
+ 
+ 
+ 
+ 
+client.login("NDgwNDc5NjE3MjM3NTE2Mjk5.Ds2RtA.ba4wJPnEM5GLdIAda19auHoFJMk");
